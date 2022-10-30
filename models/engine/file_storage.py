ec1760903a96a2146a9f6aca7a552fa9877cf4c9
hbadejo <badejohakeem95@gmail.com>
2022-10-30 14:38:34 +0100

    Pycodestyle on file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,15 +20,15 @@ class FileStorage():
 
     Class Attributes:
         __file_path: string - path to the JSON file (ex: file.json)
-        __objects: dictionary - empty but will store all objects by <class name>.id
-            (ex: to store a BaseModel object with id=12121212, the key will be BaseModel.12121212)
+        __objects: dictionary - empty but will store all
+            objects by <class name>.id
 
     Class Methods:
         all(self): returns the dictionary __objects
         new(self, obj): sets in __objects the obj with key <obj class name>.id
         save(self): serializes __objects to the JSON file (path: __file_path)
         reload(self): deserializes the JSON file to __objects
-            (only if the JSON file (__file_path) exists ; otherwise, do nothing.
+            (only if the JSON file (__file_path) exists; otherwise, do nothing.
             If the file doesn’t exist, no exception should be raised)
     """
     __file_path = "file.json"
@@ -57,8 +57,8 @@ class FileStorage():
 
     def reload(self):
         """
-        Deserializes the JSON file to __objects 
-        (only if the JSON file (__file_path) exists ; otherwise, do nothing. 
+        Deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists ; otherwise, do nothing.
         If the file doesn’t exist, no exception should be raised)
         """
         try:
