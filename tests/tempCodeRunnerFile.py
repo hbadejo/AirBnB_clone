9ee5fec4e2f7c81de18114f2f4872d957c569b05
hbadejo <badejohakeem95@gmail.com>
2022-10-30 22:07:55 +0100

    Unitest for models

---
+++ tests/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+:
+        h = "EOF signal to terminal instance for the program"
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help EOF"))
+            self.assertEqual(h, output.getvalue().strip())
